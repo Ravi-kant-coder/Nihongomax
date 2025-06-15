85e4be120a307df94cf0033c5b4db6dd6e83b036
Ravi Kant gupta <ravijiro@gmail.com>
2025-06-15 18:19:51 +0530

    messages avatars added

--- components/MsgBox.jsx
+++ components/MsgBox.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
+import Msg from "./Msg";
 
 const MsgBox = () => {
   const [showModal, setShowModal] = useState(false);
@@ -12,34 +13,44 @@ const MsgBox = () => {
           initial={{ scale: 0, rotate: -50, x: -50 }}
           animate={{ scale: 1, rotate: 0, x: 0 }}
           transition={{ type: "spring", stiffness: 300 }}
-          className="fixed md:bottom-0 bottom-20 md:left-40 md:w-1/4 left-1 right-1 backdrop-blur-[2px] md:h-2/3 top-30 md:top-auto"
+          className="fixed bg-white dark:bg-[rgb(20,20,20)] dark:text-white md:bottom-0 rounded-md bottom-18 md:left-40 md:w-1/4 left-1 right-1 md:h-2/3 top-26 overflow-y-auto md:top-auto"
         >
-          <div className=" bg-black/60 h-full dark:bg-[rgb(30,30,30)] rounded p-6 md:space-y-4 space-y-2  dark:border-gray-200">
-            <div className=" text-xl font-semibold">
-              <span>Test</span>
-            </div>
+          <div className=" md:space-y-4 space-y-2  dark:border-gray-200">
+            <Msg />
             <button
               onClick={() => setShowMsgBox(false)}
-              className="px-4 py-2 rounded-lg bg-gray-300  cursor-pointer dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-sm"
+              className="bg-gray-300 cursor-pointer dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-sm"
             >
               Cancel
             </button>
-            <p className="text-white">Requirements</p>
-            <div className="text-xs dark:text-gray-400 md:space-y-2 space-y-1">
-              <div className="flex items-center"></div>
-              <div className="flex items-center"></div>
-              <div className="flex items-center"></div>
-              <div className="flex items-center"></div>
-              <div className="flex items-center text-sm">Job Description</div>
-            </div>
             <div className="flex items-center justify-between text-sm">
               <button
                 onClick={() => setShowModal(true)}
                 className="mt-4 bg-red-400 dark:bg-red-900 cursor-pointer dark:hover:bg-red-700 hover:bg-red-500  hover:text-white py-2 px-4 rounded font-medium"
               >
-                Delete this Post?
+                Delete
               </button>
             </div>
+            <div className="flex items-center">sdf</div>
+            <div className="flex items-center">xcb</div>
+            <div className="flex items-center">wert</div>
+            <div className="flex items-center">5467</div>
+            <div className="flex items-center">sdf</div>
+            <div className="flex items-center">xcb</div>
+            <div className="flex items-center">wert</div>
+            <div className="flex items-center">5467</div>{" "}
+            <div className="flex items-center">sdf</div>
+            <div className="flex items-center">xcb</div>
+            <div className="flex items-center">wert</div>
+            <div className="flex items-center">5467</div>{" "}
+            <div className="flex items-center">sdf</div>
+            <div className="flex items-center">xcb</div>
+            <div className="flex items-center">wert</div>
+            <div className="flex items-center">5467</div>{" "}
+            <div className="flex items-center">sdf</div>
+            <div className="flex items-center">xcb</div>
+            <div className="flex items-center">wert</div>
+            <div className="flex items-center">5467</div>
           </div>
           {showModal && (
             <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30">
